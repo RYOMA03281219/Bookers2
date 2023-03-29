f8f163fa047bc8b5c853f87e4029705047b9d53e
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-03-29 13:13:55 +0000

    [Add]5 createアクションの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    @book.useer_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
   end
 
   def edit
