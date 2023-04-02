b0196fcda01092e8a6e45aabe158a4496a69371f
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-02 16:06:18 +0000

    [Update]5 Updateアクションの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
   def update
     @user= User.find(params[:id])
-    @user.update(user_params)
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
     else
@@ -23,9 +22,9 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:name, :profile_image)
+      params.require(:user).permit(:name, :profile_image, :introduction)
     end
-    
+
     def ensure_currect_user
       @user = User.find(params[:id])
       unless @user == current_user
